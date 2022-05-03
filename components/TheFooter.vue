a39e779abc9c2ab3172d4b62a178991b60ceec7e
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-05-03 23:13:13 +0300

    refactor: removed extra div and margin

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,20 +2,21 @@
 </script>
 
 <template>
-    <div class="the-footer">
-        <!-- built with nuxt3 and typescript -->
-        <p class="the-footer-text">
-            Crafted with curiosity and attentiveness.
-            <br>
-            Built with
-            <FooterLink url="https://v3.nuxtjs.org/" title="Nuxt3" color="var(--nuxt3-logo-color)" />
-            and
-            <FooterLink url="https://www.typescriptlang.org/" title="TypeScript" color="var(--typescript-logo-color)" />.
-            <br>
-            Mike Zamayias, {{ new Date().getFullYear() }}.
-        </p>
-    </div>
+    <!-- built with nuxt3 and typescript -->
+    <p class="the-footer-text">
+        Crafted with curiosity and attentiveness.
+        <br>
+        Built with
+        <FooterLink url="https://v3.nuxtjs.org/" title="Nuxt3" color="var(--nuxt3-logo-color)" />
+        and
+        <FooterLink url="https://www.typescriptlang.org/" title="TypeScript" color="var(--typescript-logo-color)" />.
+        <br>
+        Mike Zamayias, {{ new Date().getFullYear() }}.
+    </p>
 </template>
 
 <style scoped>
+.the-footer-text {
+    margin: 0;
+}
 </style>
