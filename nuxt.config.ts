b158e07d778a5ffdfa65bc14b78bc3e931671823
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-05-03 22:45:38 +0300

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,26 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    css: [
+        '~/assets/css/main.css',
+    ],
+    plugins: [
+        '~/plugins/font-awesome.ts',
+    ],
+    components:[
+        '~/components',
+        '~/components/index/',
+    ],
+    build: {
+        transpile: [
+            "@fortawesome/fontawesome-svg-core",
+            "@fortawesome/free-brands-svg-icons",
+            "@fortawesome/free-regular-svg-icons",
+            "@fortawesome/free-solid-svg-icons",
+            "@fortawesome/vue-fontawesome",
+        ]
+    }
 })
